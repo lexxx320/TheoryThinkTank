36082d7e30b0cd499608860379d25c0a63520915
Matthew Le <ml9951@matthews-mbp.wireless.rit.edu>
2014-09-12 11:17:24 -0400

    finishing reduction

--- 3sat_reduction.v
+++ 3sat_reduction.v
@@ -2,7 +2,7 @@
 http://dl.acm.org/citation.cfm?id=976579
 *)
 
-Require Export Omega.   
+Require Export Omega.             
 Require Export Bool.
 Require Export List.
 Export ListNotations.
@@ -57,14 +57,14 @@ Inductive SAT : @env bool -> bformula -> Prop :=
 (*specification of graph coloring*)
 Inductive coloring : @env colors -> graph -> nat -> Prop :=
 |cgempty : forall eta C, coloring eta emptyGraph C 
-|cgVertex : forall eta C C' G v, C <= C' -> coloring (consEnv v C' eta) G C ->
+|cgVertex : forall eta C C' G v, C' <= C -> coloring (consEnv v C' eta) G C ->
                           coloring eta (newVertex v G) C
-|cgEdge : forall eta C1 C2 C A B G, C1 <= C2 -> C2 <= C -> C1 <> C2 -> 
-                             coloring (consEnv A C1 (consEnv B C2 eta)) G C ->
+|cgEdge : forall eta C1 C2 C A B G, lookup eta A = Some C1 -> lookup eta B = Some C2 ->
+                             C1 <= C -> C2 <= C -> C1 <> C2 -> coloring eta G C ->
                              coloring eta (newEdge (A, B) G) C
 |cgUnion : forall eta G1 G2 C, coloring eta G1 C -> coloring eta G2 C ->
                         coloring eta (gunion G1 G2) C. 
-             
+
 Fixpoint newV vs G :=
   match vs with
       |v::vs => newVertex v (newV vs G)
@@ -94,8 +94,40 @@ Inductive vars_to_clique : list (bvar * vvar * vvar * vvar) -> list bvar ->
          connectV Gamma Delta x G4 ->
          vars_to_clique Gamma (x::Delta) (gunion G1 (gunion G2 (gunion G3 G4))). 
 
+Inductive clique : list (bvar * vvar * vvar * vvar) -> list bvar ->
+                   graph -> Prop :=
+|clique_empty : forall Gamma, clique Gamma nil emptyGraph
+|clique_vtx : forall Gamma u v v' x Delta G1 G2, 
+                In (u,v,v',x) Gamma -> clique Gamma Delta G1 -> connectX Gamma Delta x G2 ->
+                clique Gamma (u::Delta) (gunion G1 G2). 
+
+(*convert base (Gamma; Delta |- C Downarrow G*)
+Inductive convert_base : list (bvar * vvar * vvar * vvar) -> list bvar ->
+                         colors -> graph -> Prop :=
+|conv'''_base : forall Gamma C, convert_base Gamma nil C emptyGraph
+|conv'''_cont : forall Gamma Delta u v v' x C G,
+                  In (u,v,v',x) Gamma -> convert_base Gamma Delta C G ->
+                  convert_base Gamma (u::Delta) C (newEdge (C, v) (newEdge (C, v') G)). 
+
+(*mkEdge c u v v' e*)
+Inductive mkEdge : vvar -> bformula -> vvar -> vvar -> edge -> Prop :=
+|mkPos : forall c u v v', mkEdge c (pos u) v v' (c, v)
+|mkNeg : forall c u v v', mkEdge c (neg u) v v' (c, v'). 
+
+Fixpoint newE es G :=
+  match es with
+      |e::es => newEdge e (newE es G)
+      |nil => G
+  end. 
+
 Inductive convFormula : list (bvar * vvar * vvar * vvar) -> list bvar ->
                         bformula -> graph -> Prop := 
+|conv'' : forall Gamma Delta u1 u2 u3 v1 v2 v3 v1' v2' v3' x1 x2 x3 c G e1 e2 e3 p1 p2 p3, 
+            In (u1,v1,v1',x1) Gamma -> In (u2,v2,v2',x2) Gamma -> In (u3,v3,v3',x3) Gamma ->
+            convert_base Gamma Delta c G -> mkEdge c p1 v1 v1' e1 -> mkEdge c p2 v2 v2' e2 ->
+            mkEdge c p3 v3 v3' e3 -> 
+            convFormula Gamma (u1::u2::u3::Delta) (disjForm p1 (disjForm p2 p3))
+                        (newVertex c (newE [e1;e2;e3] G))
 . 
 
 (*Convert Stack of Continuations (Gamma; Delta |- K => G)*)
@@ -115,7 +147,10 @@ Inductive reduce : list (bvar * vvar * vvar * vvar) -> list bvar ->
 |convConj : forall Gamma Delta K F F' C C' G, 
               reduce Gamma Delta (F::K) F' C C' G ->
               reduce Gamma Delta K (conjForm F F') C C' G
-.
+|convV : forall Gamma Delta K F1 F2 F3 G1 G2 G3 C,
+         convStack Gamma Delta ((disjForm F1 (disjForm F2 F3))::K) G1 ->
+         clique Gamma Delta G2 -> vars_to_clique Gamma Delta G3 ->
+         reduce Gamma Delta K (disjForm F1 (disjForm F2 F3)) C C (gunion G1 (gunion G2 G3)).
 
 Theorem reductionInvariant : forall K F G C C' Delta Gamma, 
                                reduce Gamma Delta K F C C' G -> C <= C'. 
