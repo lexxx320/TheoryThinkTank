957387c11fb09e44de12c1f0e4da9126e6218fb2
Matthew Le <ml9951@Matthews-MacBook-Pro-2.local>
2014-09-24 21:54:51 -0400

    updating

--- borda_two_manipulators_npc/borda.v
+++ borda_two_manipulators_npc/borda.v
@@ -1,50 +1,11 @@
-
-Inductive ilist {A:Set} : nat -> Set :=
-| iNil : ilist O
-| iCons : forall n, A -> ilist n -> ilist (S n).
-
-Fixpoint app' {A:Set} n1 (ls1 : @ilist A n1) n2 (ls2 : ilist n2) : ilist (n1 + n2) :=
-    match ls1 in (ilist n1) return (ilist (n1 + n2)) with
-      | iNil => ls2
-      | iCons _ x ls1' => iCons _ x (app' _ ls1' _ ls2)
-    end.
-
-Fixpoint addLists n (l1:@ilist nat n) (l2:@ilist nat n) : ilist n :=
-  match l1 in (ilist n), l2 in (ilist n) return (ilist n) with
-      |iCons a h1 t1, iCons b h2 t2 => iCons _ (h1+h2) (addLists _ t1 t2)
-      |_, _ => iNil
-  end. 
-
 (*An attempt to formalize the result presented in: 
 **"Complexity of and Algorithms for Borda Manipulation" - 
 **http://www.cs.toronto.edu/~jdavies/daviesAAAI11.pdf*)
 
 Require Export Coq.Sorting.Permutation. 
-Require Export Omega.     
-(*Require Export List.
-Export ListNotations. *)
-
-(*indexed lists based on material from http://adam.chlipala.net/cpdt/html/MoreDep.html*)
-Inductive ilist {A:Type} : nat -> Type :=
-  | iNil : ilist O
-  | iCons : forall n, A -> ilist n -> ilist (S n).
-
-Notation " [ ] " := iNil : list_scope.
-Notation " [ x ] " := (iCons x iNil) : list_scope.
-Notation " [ x ; .. ; y ] " := (iCons x .. (iCons y iNil) ..) : list_scope.
-
-Fixpoint app' n1 (ls1 : ilist n1) n2 (ls2 : ilist n2) : ilist (n1 + n2) :=
-    match ls1 in (ilist n1) return (ilist (n1 + n2)) with
-      | iNil => ls2
-      | iCons _ x ls1' => iCons x (app' ls1' ls2)
-    end.
-
-Fixpoint iapp {A:Type} (n1:nat) (l1 : ilist n1) (n2:nat) (l2 : ilist n2) : ilist (n1+n2) :=
-  match l1 in ilist n1 return (ilist (n1 + n2)) with
-      |iCons n1' hd tl => iCons (n1'+n2) hd (iapp n1' tl n2 l2)
-      |iNil => l2
-  end. 
-
+Require Export Omega.   
+Require Export List. 
+Export ListNotations. 
 
 Definition optGTE (n:nat) (o:option nat) : Prop :=
   match o with
@@ -94,6 +55,9 @@ Inductive reduceMid (n:nat) (C:nat) : list nat -> list nat -> Prop :=
      reduce l (C::l'++[2*(n+2) + C; y]
 
 *)
+
+
+
 Inductive reduce : list nat -> list nat -> Prop :=
 |reduce_ : forall C y l l' n, y <= C -> n = length l -> reduceMid n C l l' ->
                          reduce l (C:: (l' ++ [4+2*n+C; y])).
@@ -104,6 +68,29 @@ Inductive allLTE (i:nat) : list nat -> Prop :=
 
 Inductive firstCandidateWins : list nat -> Prop :=
 |firstCandidateWins_ : forall hd tl, allLTE hd tl -> firstCandidateWins (hd::tl). 
+
+Theorem lengthEqCons : forall (A:Type)(h1 h2 :A)t1 t2, length(h1::t1) = length (h2::t2) ->
+                                                  length t1 = length t2. 
+Proof. intros. simpl in H. inversion H. reflexivity. Qed. 
+
+Theorem lengthsNeq : forall (A:Type) (h:A) t, length (h::t) = length (@nil A) -> False. 
+Proof.
+  intros. inversion H. Qed.
+
+Theorem lengthsNeq' : forall (A:Type) (h:A) t, length (@nil A) = length (h::t) -> False. 
+Proof.
+  intros. inversion H. Qed.
+ 
+Fixpoint addVecs (v1:list nat) (v2:list nat) : length v1 = length v2 -> list nat :=
+  match v1 as v1', v2 as v2' with
+    |h1::t1, h2::t2 => (fun prf : length (h1::t1) = length (h2::t2) => 
+                         h1+h2::addVecs t1 t2 (lengthEqCons nat h1 h2 t1 t2 prf))
+    |nil, nil => fun _ => nil
+    |h::t, nil => fun prf : length (h::t)=length nil => 
+                      match lengthsNeq nat h t prf with end
+    |nil, h::t => fun prf : length nil=length (h::t) =>
+                   match lengthsNeq' nat h t prf with end
+  end. 
 
 Inductive addThreeVectors : list nat -> list nat -> list nat -> list nat -> Prop :=
 |vAddNil : addThreeVectors nil nil nil nil
@@ -127,13 +114,13 @@ Ltac copy H :=
       |?X => assert(X) by auto
   end. 
 
-Theorem eqLengthsVecAdd : forall l1 l2 l3, length l1 = length l2 -> length l2 = length l3 ->
-                                      exists l, addThreeVectors l1 l2 l3 l. 
+Theorem existsVecAdd : forall v1 v2 v3, length v1 = length v2 -> length v2 = length v3 ->
+                                   exists v4, addThreeVectors v1 v2 v3 v4. 
 Proof.
-  induction l1; intros. 
-  {destruct l2; destruct l3; simpl in *; try omega. exists nil. constructor. }
-  {destruct l2; destruct l3; simpl in *; try omega. inv H. inv H0. apply IHl1 in H1; auto. 
-   invertHyp. exists (a+n+n0::x). constructor. auto. }
+  induction v1; intros. 
+  {destruct v2; destruct v3; simpl in *; try omega. exists nil. constructor. }
+  {destruct v2; destruct v3; simpl in *; try omega. inv H. inv H0. 
+   apply IHv1 in H1; auto. invertHyp. exists (a+n+n0::x). constructor. auto. }
 Qed. 
 
 Theorem reduceMidEqLengths : forall l C l' n, reduceMid n C l l' -> length l = length l'. 
@@ -141,46 +128,46 @@ Proof.
   intros. induction H; auto. simpl. rewrite IHreduceMid. auto. 
 Qed. 
 
-Lemma zgtz : 0 > 0 -> False.
-  omega. 
-Qed.
 
-Definition pred_strong2 (s : {n : nat | n > 0}) : nat :=
-  match s with
-    | exist O pf => match zgtz pf with end
-    | exist (S n') _ => n'
-  end.
-
-Theorem consLengthEq : forall (A:Type) (h1 h2 :A) t1 t2,
-                         length (h1::t1) = length (h2::t2) -> length t1 = length t2. 
-Proof. intros. inv H. auto. 
+Theorem addVecsApp : forall l1 l1' l2 l2' l3 l3' l4,
+                     length l1 = length l2 -> length l2 = length l3 ->
+                     addThreeVectors (l1++l1') (l2++l2') (l3++l3') l4 ->
+                     exists v v', 
+                     addThreeVectors l1 l2 l3 v /\ addThreeVectors l1' l2' l3' v' /\
+                     v++v' = l4. 
+Proof.
+  induction l1; intros. 
+  {destruct l2; destruct l3; simpl in *; try omega. exists nil. exists l4. split. constructor. 
+   split. auto. simpl. auto. }
+  {destruct l2; destruct l3; simpl in *; try omega. inv H1. inv H. inv H0. 
+   apply IHl1 in H9; auto.invertHyp. exists (a+n+n0::x). exists x0. split. constructor. 
+   auto. split; auto. }
+Qed. 
+                                                                         
+Theorem allLTEApp : forall n l1 l2, allLTE n l1 -> allLTE n l2 ->
+                               allLTE n (l1++l2). 
+Proof.
+  intros. induction H. simpl. auto. simpl. constructor; auto. 
 Qed. 
 
-Fixpoint vecAdd v1 v2 (prf:length v1 = length v2) :=
-  match v1 with
-      |h1::t1 => match v2 with
-                    |h2::t2 => h1+h2::vecAdd t1 t2 (consLengthEq nat h1 h2 t1 t2 prf)
-                    |_ => nil
-                end
-      |_ => nil
-  end. 
-
-
-, v2 with
-      |h1::t1, h2::t2 => 
-      |_,_ => nil
-  end. 
-
-Theorem BordaTwoManipulatorsNPC : forall l l', reduce l l' -> (PermSum l <-> twoManipulation l').
+Theorem BordaTwoManipulatorsNPC : forall l l', reduce l l' ->
+                                          (PermSum l <-> twoManipulation l').
 Proof.
   intros. split; intros. 
-  {inv H. inv H0. invertHyp. copy H3.
+  {inv H. inv H0. invertHyp. copy H3. apply reduceMidEqLengths in H4. 
+   copy H0. copy H2. apply Permutation_length in H6. apply Permutation_length in H7.
+   assert(exists v, addThreeVectors (2 + length l::x++[0; S (length l)])
+                               (2 + length l::x0++[0; S (length l)]) 
+                               (C::l'0++[4+2*(length l)+C; y]) v). 
+   apply existsVecAdd; auto. simpl. repeat rewrite app_length. simpl. 
+   rewrite H6. rewrite H7. auto. simpl. repeat rewrite app_length. simpl. 
+   rewrite H7. rewrite H4. auto. invertHyp. econstructor. eauto. 
+   inv H9. simpl. repeat rewrite <- plus_n_Sm. constructor. 
+   apply addVecsApp in H16; try omega. invertHyp. inv H9. inv H17. inv H16. 
+   apply allLTEApp. Focus 2. constructor. omega. constructor. omega. constructor. 
    
 
 
-   eapply twoManipulation_ with (v1 := 2 + length l::x++[0; S (length l)])
-                                  (v2 := 2 + length l::x0++[0; S (length l)]).
-   eapply eqLengthsVecAdd. 
 
 
 
